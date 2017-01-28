41c6f5d0e4d97fafc8e86d5616284f51db7fc439
AccidentalRebel <accidentalrebel@gmail.com>
2017-01-28 10:54:53 +0800

    Messages have a prefix of Emacs-dokuwiki:

--- emacs-dokuwiki.el
+++ emacs-dokuwiki.el
@@ -6,31 +6,26 @@
 
 (require 'xml-rpc)
 
-;; (defvar *dokuwiki-xml-rpc-url* "http://wiki.gamedevph.com/lib/exe/xmlrpc.php")
-;; (xml-rpc-method-call
-;; *emacs-dokuwiki-xml-rpc-url*
-;; 'wiki.getPage "start")
-
 (defvar *emacs-dokuwiki-xml-rpc-url* "")
 
 (defun emacs-dokuwiki-setup()
   "Initial set up for the dokuwiki"
   (interactive)
   (let ((xml-rpc-url (read-string "Enter wiki URL: ")))
-    (message "Saved the wiki url: %s." xml-rpc-url)
+    (message "emacs-dokuwiki: Saved the wiki url: %s." xml-rpc-url)
     (setq *emacs-dokuwiki-xml-rpc-url* xml-rpc-url)))
 
 (defun emacs-dokuwiki-open-page()
   "Connects to the dokuwiki"
   (interactive)
   (if (equal *emacs-dokuwiki-xml-rpc-url* "")
-      (user-error "Call emacs-dokuwiki-setup() first")
+      (user-error "Emacs-dokuwiki: Call emacs-dokuwiki-setup() first")
     (let ((page-name (read-string "Enter page name: ")))
       (message "page name is %s" page-name)
       (let ((page-content (xml-rpc-method-call *emacs-dokuwiki-xml-rpc-url* 'wiki.getPage page-name)))
 	(if (equal page-content nil)
-	    (error "Could not get the page content from page %s" page-name)
-	  (message "Creating a new buffer for page %s" page-name)
+	    (error "Emacs-dokuwiki: Could not get the page content from page %s" page-name)
+	  (message "Emacs-dokuwiki: Creating a new buffer for page %s" page-name)
 	  (switch-to-buffer (concat "emacs-dokuwiki:" page-name))
 	  (insert page-content))))))
 
@@ -38,15 +33,15 @@
   "Gets the title of the current wiki"
   (interactive)
   (if (equal *emacs-dokuwiki-xml-rpc-url* "")
-      (user-error "Call emacs-dokuwiki-setup() first")
+      (user-error "Emacs-dokuwiki: Call emacs-dokuwiki-setup() first")
     (let ((dokuwiki-title (xml-rpc-method-call *emacs-dokuwiki-xml-rpc-url* 'dokuwiki.getTitle)))
-      (message "The title of the wiki is %s" dokuwiki-title))))
+      (message "Emacs-dokuwiki: The title of the wiki is %s" dokuwiki-title))))
 
 (defun emacs-dokuwiki-login()
   "Connects to the dokuwiki"
   (interactive)
   (if (equal *emacs-dokuwiki-xml-rpc-url* "")
-      (user-error "Call emacs-dokuwiki-setup() first")
+      (user-error "Emacs-dokuwiki: Call emacs-dokuwiki-setup() first")
     (let* ((login-name (read-string "Enter login name: "))
 	   (login-password (read-passwd "Enter password: ")))
       (xml-rpc-method-call *emacs-dokuwiki-xml-rpc-url* 'dokuwiki.login login-name login-password))))
